f096d35971ecfcd855fefcb255d472480cb631ad
MeetBudheliya <Meetbudheliya04@gmail.com>
2021-03-25 09:40:18 +0530

    25/03/2021

--- shadowView/ViewController.swift
+++ shadowView/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var viewHeight: NSLayoutConstraint!
+    @IBOutlet weak var expand: UIButton!
+    var isExpand = false
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        expand.layer.cornerRadius = 10
     }
 
-
+    @IBAction func changeExpandClicked(_ sender: UIButton) {
+        if isExpand{
+            viewHeight.constant = 350
+            isExpand = false
+        }else{
+            viewHeight.constant = 200
+            isExpand = true
+        }
+    }
+    
 }
 
