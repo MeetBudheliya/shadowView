e7a4352152341d30038d1cb49b66cc246f2850a6
MeetBudheliya <Meetbudheliya04@gmail.com>
2021-03-25 10:15:30 +0530

    25/03/2021_1

--- shadowView/ViewController.swift
+++ shadowView/ViewController.swift
@@ -12,10 +12,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var viewHeight: NSLayoutConstraint!
     @IBOutlet weak var expand: UIButton!
+
+    @IBOutlet weak var vieww: viewShadow!
     var isExpand = false
     override func viewDidLoad() {
         super.viewDidLoad()
+        vieww.layer.masksToBounds = false
+        vieww.layer.shadowColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+            vieww.layer.shadowOffset = CGSize(width: 2, height: 2)
+        vieww.layer.shadowOpacity = 0.5
+        vieww.layer.shadowRadius = 7
         
+        vieww.layer.cornerRadius = 10
         expand.layer.cornerRadius = 10
     }
 
